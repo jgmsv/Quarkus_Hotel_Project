09e29b02fe0cca5e3ac700ccf5ea0a4e603d284c
JgV <jose.guilherme.vieira@gmail.com>
2024-02-05 00:01:24 +0000

    setUp method to drop collection beforeEach test.

--- src/test/java/org/mindera/system/hotelServiceTest.java
+++ src/test/java/org/mindera/system/hotelServiceTest.java
@@ -1,14 +1,17 @@
 package org.mindera.system;
 
+import com.mongodb.client.MongoClient;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mindera.dto.hotel.CreateHotelDto;
 import org.mindera.dto.hotel.CreateRoomDto;
 import org.mindera.model.hotel.Hotel;
 import org.mindera.model.hotel.RoomType;
 import org.mindera.repository.HotelRepository;
+import org.mindera.service.hotel.HotelService;
 
 import java.util.Set;
 
@@ -20,7 +23,18 @@ import static org.hamcrest.Matchers.is;
 public class hotelServiceTest {
 
     @Inject
+    MongoClient mongoClient;
+    @Inject
     HotelRepository hotelRepository;
+    @Inject
+    HotelService hotelService;
+
+    @BeforeEach
+    public void setUp() {
+        mongoClient.getDatabase("hoteldb").getCollection("Hotels").drop();
+
+    }
+
 
     @Test
     public void testAddHotel() {
@@ -30,9 +44,6 @@ public class hotelServiceTest {
 
         CreateHotelDto createHotelDto = new CreateHotelDto("Test Hotel", "Test Location", 123456789,
                 Set.of(new CreateRoomDto(1, 2, RoomType.SINGLEROOM, 100)));
-
-        // When
-
         // When
         given()
                 .contentType(ContentType.JSON)
@@ -40,16 +51,20 @@ public class hotelServiceTest {
                 .when().post("/api/v1/hotel")
                 .then()
                 .statusCode(200);
-
         // Then
         Hotel hotel = hotelRepository.findByHotelN("Test Hotel").orElse(null);
         assert hotel != null;
-        // Add more assertions based on your requirements
-        // For example, you might want to check if the hotel properties match the DTO properties
         assertThat(hotel.getLocation(), is("Test Location"));
         assertThat(hotel.getPhoneNumber(), is(123456789));
-        // Also, check if the room information is persisted
         assertThat(hotel.getRooms().size(), is(1));
-        // Add more assertions for room details if needed
+
+    }
+
+    @Test
+    public void testAddHotelDuplicate() {
+        CreateHotelDto createHotelDto = new CreateHotelDto("Duplicate Hotel", "Location", 123456789, null);
+        Hotel duplicateHotel = Hotel.builder().hotelN("Duplicate Hotel").location("Location").phoneNumber(123456789).rooms(null).build();
+
+
     }
 }
